7ffe78f3cbfa0e620fd1690a891138f085b21e76
Arkadiusz Wołk <arkadiusz.wolk@cern.ch>
2021-08-17 12:20:15 +0200

    runs on CRAB

---
+++ Analyzer/DiamondTimingAnalyzer/python/diamond_timing_worker_cfi.py
@@ -0,0 +1,14 @@
+import FWCore.ParameterSet.Config as cms
+from DQMServices.Core.DQMEDAnalyzer import DQMEDAnalyzer
+
+diamondTimingWorker = DQMEDAnalyzer("DiamondTimingWorker",
+    tagDigi = cms.InputTag("ctppsDiamondRawToDigi", "TimingDiamond"),
+    tagRecHit = cms.InputTag("ctppsDiamondRecHits"),
+    tagPixelLocalTrack = cms.InputTag("ctppsPixelLocalTracks"),
+    tagLocalTrack = cms.InputTag("ctppsDiamondLocalTracks"), 
+    tagCalibrationFile = cms.string("src/Analyzer/DiamondTimingAnalyzer/python/DiamondCalibration.json"),
+    tagValidOOT = cms.int32(-1),
+    folder = cms.string("myfolder"),
+    Ntracks_Lcuts = cms.vint32([-1,1,-1,1]),
+    Ntracks_Ucuts = cms.vint32([-1,6,-1,6]),
+)
